11820079c51faf7bb8e773812c7998d98af759ec
boburjonurazbayev <bobur7850@gmail.com>
2021-12-12 05:58:30 +0500

    Initial commit

---
+++ css/main.js
@@ -0,0 +1,6 @@
+const elHamburger = document.querySelector ('.header__menu-button')
+const elHeader = document.querySelector ('.header')
+
+elHamburger .addEventListener('click', () => {
+    elHeader.classList.toggle('header--active')
+})
