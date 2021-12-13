c7629e7aa28d6f397ce0cea36471071de0df7681
boburjonurazbayev <bobur7850@gmail.com>
2021-12-13 19:31:47 +0500

    Change hamburger bug

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-const elHamburger = document.querySelector ('.hamburger')
-const elHeader = document.querySelector ('.header')
+const elHamburger = document.querySelector ('.hamburger');
+const elHamburgerS = document.querySelector ('.hamburger--second');
+const elHeader = document.querySelector ('.header');
 
 elHamburger .addEventListener('click', () => {
+    elHeader.classList.toggle('header--active')
+})
+
+elHamburgerS .addEventListener('click', () => {
     elHeader.classList.toggle('header--active')
 })
